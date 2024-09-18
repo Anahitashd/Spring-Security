8a647ef971ae2ed4ec7cce689dcb1083b5a86f3f
Anahitashd <155881531+Anahitashd@users.noreply.github.com>
2024-09-18 10:06:50 +0330

    Update ApplicationUserRole.java

--- demo/src/main/java/com/example/demo/security/ApplicationUserRole.java
+++ demo/src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -1,6 +1,23 @@
 package com.example.demo.security;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.example.demo.security.ApplicationUserPermission.*;
+
 public enum ApplicationUserRole {
-    STUDENT,
-    ADMIN
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_WRITE, COURSE_READ, STUDENT_WRITE, STUDENT_READ)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
+
+    private final Set<ApplicationUserPermission> permission;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permission) {
+        this.permission = permission;
+    }
+
+    public Set<ApplicationUserPermission> getPermission() {
+        return permission;
+    }
 }
